9533258d70aae0e06870788d955f1c2c2c23aa5b
leandro.tavares <leandro.tavares@original.com.br>
2022-03-02 14:26:53 -0300

    tenis amount

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -11,6 +11,7 @@ interface Product {
   title: string;
   price: number;
   image: string;
+  amount: number
 }
 
 interface ProductFormatted extends Product {
@@ -25,9 +26,12 @@ const Home = (): JSX.Element => {
   const [products, setProducts] = useState<ProductFormatted[]>([]);
   const { addProduct, cart } = useCart();
 
-  // const cartItemsAmount = cart.reduce((sumAmount, product) => {
-  //   // TODO
-  // }, {} as CartItemsAmount)
+  const cartItemsAmount = cart.reduce((sumAmount, product) => {
+    sumAmount[product.id] = product.amount
+    console.log(sumAmount);
+    return sumAmount;
+    
+  }, [] as CartItemsAmount)
 
   useEffect(() => {
     async function loadProducts() {
@@ -60,7 +64,7 @@ const Home = (): JSX.Element => {
           >
             <div data-testid="cart-product-quantity">
               <MdAddShoppingCart size={16} color="#FFF" />
-              {/* {cartItemsAmount[product.id] || 0} */} 2
+              {cartItemsAmount[product.id] || 0}
             </div>
 
             <span>ADICIONAR AO CARRINHO</span>
